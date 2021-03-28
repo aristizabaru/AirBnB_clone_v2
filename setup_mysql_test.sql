33d7ed6c96bed0ec39db39dab1a842d500aada32
aristizabaru <andres.aristizabal@gmail.com>
2021-03-28 14:32:00 +0000

    First commit

---
+++ setup_mysql_test.sql
@@ -0,0 +1,9 @@
+-- Prepares a MySQL server
+--    - creates database, new user and set password for test
+--    - grant access to new user to hbnb_test_db and performance_schema DB
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
