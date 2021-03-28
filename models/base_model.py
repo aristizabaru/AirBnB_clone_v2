daa9d63e900eb55e620cea1cc45b89696ad06597
aristizabaru <andres.aristizabal@gmail.com>
2021-03-28 14:02:59 +0000

    Fix: get kwards parameters in constructor

--- models/base_model.py
+++ models/base_model.py
@@ -3,9 +3,12 @@
 import uuid
 from datetime import datetime
 
+_format = "%Y-%m-%dT%H:%M:%S.%f"
+
 
 class BaseModel:
     """A base class for all hbnb models"""
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
@@ -15,12 +18,45 @@ class BaseModel:
             self.updated_at = datetime.now()
             storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            # set attributes
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+
+            # compare created_at
+            if kwargs.get("created_at", None):
+                self.created_at = datetime.strptime(
+                    kwargs["created_at"], _format)
+            else:
+                self.created_at = datetime.utcnow()
+
+            # compare updated_at
+            if kwargs.get("updated_at", None):
+                self.updated_at = datetime.strptime(
+                    kwargs["updated_at"], _format)
+            else:
+                self.updated_at = datetime.utcnow()
+
+            # compare id and save new instance
+            if kwargs.get("id", None) is None:
+                from models import storage
+                self.id = str(uuid.uuid4())
+                storage.new(self)
+
+        # if kwargs:
+        #     for key in kwargs:
+        #         if key != "__class__":
+        #             if key == "created_at" or key == "updated_at":
+        #                 setattr(self, key, datetime.strptime(
+        #                     kwargs[key], _format))
+        #             else:
+        #                 setattr(self, key, kwargs[key])
+        #         models.storage.new(self)
+        # else:
+        #     self.id = str(uuid.uuid4())
+        #     self.created_at = datetime.now()
+        #     self.updated_at = self.created_at
+        #     models.storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
