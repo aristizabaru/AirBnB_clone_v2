53131a86c17d478c63db6bf71d8fee9ccda405b1
aristizabaru <andres.aristizabal@gmail.com>
2021-03-29 13:59:54 +0000

    Style(pep8): fix pep8 style in association_table object

--- models/place.py
+++ models/place.py
@@ -8,9 +8,11 @@ from os import getenv
 
 
 association_table = Table("place_amenity", Base.metadata,
-                          Column("place_id", String(60), ForeignKey("places.id"),
+                          Column("place_id", String(60),
+                                 ForeignKey("places.id"),
                                  primary_key=True, nullable=False),
-                          Column("amenity_id", String(60), ForeignKey("amenities.id"),
+                          Column("amenity_id", String(60),
+                                 ForeignKey("amenities.id"),
                                  primary_key=True, nullable=False))
 
 
