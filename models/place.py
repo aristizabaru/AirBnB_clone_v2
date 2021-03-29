d5c0761010de14f6b51490c62bf5dc08dec2f5d3
aristizabaru <andres.aristizabal@gmail.com>
2021-03-29 13:39:55 +0000

    Feat: create relationships

--- models/place.py
+++ models/place.py
@@ -5,6 +5,12 @@ from sqlalchemy import Column, String, ForeignKey, Integer, Float
 from sqlalchemy import Table
 from sqlalchemy.orm import relationship
 
+association_table = Table("place_amenity", Base.metadata,
+                          Column("place_id", String(60), ForeignKey("places.id"),
+                                 primary_key=True, nullable=False),
+                          Column("amenity_id", String(60), ForeignKey("amenities.id"),
+                                 primary_key=True, nullable=False))
+
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -22,27 +28,19 @@ class Place(BaseModel, Base):
     longitude = Column(Float, nullable=True)
     amenity_ids = []
 
-    metadata = Base.metadata
-    # Creating a new table for the many to many relationship
-    place_amenity = Table('place_amenity', metadata,
-        Column('place_id', String(60), ForeignKey('places.id'), primary_key=True),
-        Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True)
-    )
-
-    #Relationships bewteen tables
-    reviews = relationship("Review", backref="place")
-    amenities = relationship(
-        "Amenity", back_populates="places", secondary=place_amenity, viewonly=False
-    )
+    # Relationships bewteen tables
+    reviews = relationship("Review", backref="place", cascade="delete")
+    amenities = relationship("Amenity", secondary="place_amenity",
+                             viewonly=False)
 
     @property
     def reviews(self):
         from models import storage
         """getter attribute cities that returns the
         list of City instances with state_id"""
-        place_reviews= list()
+        place_reviews = list()
         all_reviews = storage.all("Review")
-        for key,value in all_reviews.items():
+        for key, value in all_reviews.items():
             if value.place_id == self.id:
                 place_reviews.append(value)
         return place_reviews
@@ -52,9 +50,9 @@ class Place(BaseModel, Base):
         from models import storage
         """getter attribute cities that returns the
         list of City instances with state_id"""
-        place_amenities= list()
+        place_amenities = list()
         all_amenities = storage.all("Amenity")
-        for key,value in all_amenities.items():
+        for key, value in all_amenities.items():
             if value.id in type(self).amenity_ids:
                 place_amenities.append(value)
         return place_amenities
