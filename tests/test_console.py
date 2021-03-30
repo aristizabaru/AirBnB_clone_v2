58238dfd1cb498a1de5f4a433f2ebe734a2a975e
Jumoc <juanpcaicedo2@gmail.com>
2021-03-30 00:03:49 +0000

    Feat (test_console): trying

--- tests/test_console.py
+++ tests/test_console.py
@@ -26,3 +26,26 @@ class test_Console(unittest.TestCase):
         fchecker = pep8.Checker("console.py", show_source=True)
         file_errors = fchecker.check_all()
         self.assertEqual(file_errors, 0)
+
+    def test_create(self):
+        """Check that created objects are correctly saved to the db"""
+        conn = MySQLdb.connect(
+            host="localhost", port=3306, user="hbnb_test",
+            passwd="hbnb_test_pwd", db="hbnb_test_db", charset="utf8"
+        )
+        cur = conn.cursor()
+
+        # Check amount of elements in table
+        count_before = type(self).cur.execute(
+            "SELECT count(states.id) FROM states"
+            )
+        # Create an object and upload it to the db
+        HBNBCommand.do_create('create State name="California"')
+        # Check amount of elements after adding a state
+        count_after = type(self).cur.execute(
+            "SELECT count(states.id) FROM states"
+            )
+        # Assert if count_after is greater than count_before
+        self.assertGreater(count_after, count_before)
+        cur.close()
+        conn.clos()
