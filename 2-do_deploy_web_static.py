57b507ed5ca55e073bd0924d48d1bfd78a82b705
aristizabaru <andres.aristizabal@gmail.com>
2021-04-15 12:06:38 +0000

    Fix: refactor name of file assignation

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -52,24 +52,25 @@ def do_deploy(archive_path):
     # config
     if isfile(archive_path) is False:
         return False
-    file_name = archive_path.split("/")[1]
+    file_name = archive_path.split("/")[-1]
+    name = file_name.split(".")[0]
     # put(local, remote)
     try:
         put(archive_path, "/tmp/{}".format(file_name))
-        run("mkdir -p /data/web_static/releases/{}".format(file_name[:-4]))
+        run("mkdir -p /data/web_static/releases/{}".format(name))
         run("tar -xzf /tmp/{} -C "
             "/data/web_static/releases/{}/".format(file_name,
-                                                   file_name[:-4]))
+                                                   name))
         run("rm /tmp/{}".format(file_name))
         run("mv /data/web_static/releases/{}/web_static/* "
             "/data/web_static/releases/{}/".format(
-                file_name[:-4], file_name[:-4]))
+                fname, name))
         run("rm -rf /data/web_static/releases/{}/web_static".format(
-            file_name[:-4]))
+            name))
         run("rm -rf /data/web_static/current")
         run("ln -s /data/web_static/releases/{}/ "
             "/data/web_static/current".format(
-                file_name[:-4]))
+                name))
         print("New version deployed!")
         return True
     except Exception:
