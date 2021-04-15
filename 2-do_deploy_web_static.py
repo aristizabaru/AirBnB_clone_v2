14dda212fd6ac2fea922f694924afd2dfdd08487
aristizabaru <andres.aristizabal@gmail.com>
2021-04-15 14:18:55 +0000

    refactor: commands

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -47,21 +47,19 @@ def do_deploy(archive_path):
     # put(local, remote)
     try:
         put(archive_path, "/tmp/{}".format(file_name))
-        run("rm -rf /data/web_static/releases/{}/".
-            format(name))
-        rrun("mkdir -p /data/web_static/releases/{}/".
-             format(name))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-            format(file_name, name))
+        run("mkdir -p /data/web_static/releases/{}".format(name))
+        run("tar -xzf /tmp/{} -C "
+            "/data/web_static/releases/{}/".format(file_name,
+                                                   name))
         run("rm /tmp/{}".format(file_name))
         run("rsync -a /data/web_static/releases/{}/web_static/ "
             "/data/web_static/releases/{}/".format(
                 name, name))
-        run("rm -rf /data/web_static/releases/{}/web_static".
-            format(name))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-            format(name))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(
+            name))
+        run("ln -sf /data/web_static/releases/{}/ "
+            "/data/web_static/current".format(
+                name))
         print("New version deployed!")
 
         return True
