5bf4a36eeb3944d78e1b441a0200f20c22d79cc5
aristizabaru <andres.aristizabal@gmail.com>
2021-04-16 01:35:49 +0000

    Fix: remove variables

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,18 +2,24 @@
 # Install Nginx
 if ! [ -x "$(command -v nginx)" ]
 then
-   sudo apt-get update -y
-   sudo apt-get install nginx -y
-   sudo service nginx start
+   apt-get update -y
+   apt-get install nginx -y
+   service nginx start
 fi
 
-# Create files
-mkdir -p "/data/web_static/releases/test/"
-echo -e  "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" > "/data/web_static/releases/test/index.html"
-mkdir -p "=/data/web_static/shared/"
+# Create directories and index.html
+if ! [ -f /data/web_static/releases/test/index.html ]; then
+    mkdir -p /data/web_static/releases/test/
+    echo -e  "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" > /data/web_static/releases/test/index.html
+fi
+
+# Create directory shared
+if ! [ -d /data/web_static/shared/ ]; then
+    mkdir -p /data/web_static/shared/
+fi
 
 # Create soft link from current to test directory
-ln -sf "/data/web_static/releases/test/" "/data/web_static/current"
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Change owner
 chown -R ubuntu:ubuntu /data/
@@ -22,6 +28,6 @@ chown -R ubuntu:ubuntu /data/
 IS_IN_FILE=$(grep -c hbnb_static /etc/nginx/sites-available/default)
 if [ "$IS_IN_FILE" -eq 0 ];
 then
-   sed -i "/server_name _;/ a \\\n\tlocation /hbnb_static {\\n\\t\\talias /data/web_static/current/;\\n\\t}" "/etc/nginx/sites-available/default"
+   sed -i "/server_name _;/ a \\\n\tlocation /hbnb_static {\\n\\t\\talias /data/web_static/current/;\\n\\t}" /etc/nginx/sites-available/default
    nginx -s reload
 fi
