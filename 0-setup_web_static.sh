5f1104b32acc30b99dbbe40098e4a79be9a2ef2f
aristizabaru <andres.aristizabal@gmail.com>
2021-04-16 01:41:52 +0000

    Fix: add sudo to chown

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,40 +2,27 @@
 # Install Nginx
 if ! [ -x "$(command -v nginx)" ]
 then
-   sudo apt-get update -y
-   sudo apt-get install nginx -y
-   sudo service nginx start
+   apt-get update -y
+   apt-get install nginx -y
+   service nginx start
 fi
 
-# Create directories and index.html
-FILE=/data/web_static/releases/test/index.html
-DATA="<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>"
-FILE_DIRECTORY=/data/web_static/releases/test/
-if ! [ -f "$FILE" ]; then
-    mkdir -p "$FILE_DIRECTORY"
-    echo -e  "$DATA" > "$FILE"
-fi
+# create files
+
+mkdir -p /data/web_static/releases/test/
+echo -e  "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" > /data/web_static/releases/test/index.html
 
 # Create directory shared
-DIRECTORY=/data/web_static/shared/
-if ! [ -d "$DIRECTORY" ]; then
-    mkdir -p "$DIRECTORY"
-fi
+mkdir -p /data/web_static/shared/
 
 # Create soft link from current to test directory
-LINK=/data/web_static/current
-TARGET=/data/web_static/releases/test/
-ln -sf "$TARGET" "$LINK"
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Change owner
 sudo chown -R ubuntu:ubuntu /data/
 
 # Change Nginx configuration file
-CONFIG_WEB=/etc/nginx/sites-available/default
-DATA="\\\n\tlocation /hbnb_static {\\n\\t\\talias /data/web_static/current/;\\n\\t}"
-IS_IN_FILE=$(grep -c "hbnb_static" $CONFIG_WEB)
-if [ "$IS_IN_FILE" -eq 0 ];
-then
-   sed -i "/server_name _;/ a $DATA" "$CONFIG_WEB"
-   nginx -s reload
-fi
+sed -i "/server_name _;/ a \\\n\tlocation /hbnb_static {\\n\\t\\talias /data/web_static/current/;\\n\\t}" /etc/nginx/sites-available/default
+sudo service nginx reload
+
+
