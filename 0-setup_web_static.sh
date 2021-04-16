038d18cecff7ffe56478dc5ea0275789873e47dd
aristizabaru <andres.aristizabal@gmail.com>
2021-04-16 01:51:48 +0000

    Fix: remove all checks

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,28 +1,11 @@
 #!/usr/bin/env bash
 # Install Nginx
-if ! [ -x "$(command -v nginx)" ]
-then
-   apt-get update -y
-   apt-get install nginx -y
-   service nginx start
-fi
-
-# create files
-
+apt-get update -y
+apt-get install nginx -y
 mkdir -p /data/web_static/releases/test/
-echo -e  "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" > /data/web_static/releases/test/index.html
-
-# Create directory shared
 mkdir -p /data/web_static/shared/
-
-# Create soft link from current to test directory
+echo "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Change owner
-sudo chown -R ubuntu:ubuntu /data/
-
-# Change Nginx configuration file
+chown -R ubuntu:ubuntu /data/
 sed -i "/server_name _;/ a \\\n\tlocation /hbnb_static {\\n\\t\\talias /data/web_static/current/;\\n\\t}" /etc/nginx/sites-available/default
-sudo service nginx reload
-
-
+service nginx restart
