cfe8f9ce7d37a27b8945c30c99de29413d0ff82e
aristizabaru <andres.aristizabal@gmail.com>
2021-04-16 01:31:18 +0000

    Fix: remove variables

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,16 +7,10 @@ then
    sudo service nginx start
 fi
 
-# Create directories and index.html
-if ! [ -f "/data/web_static/releases/test/index.html" ]; then
-    mkdir -p "/data/web_static/releases/test/"
-    echo -e  "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" > "/data/web_static/releases/test/index.html"
-fi
-
-# Create directory shared
-if ! [ -d "=/data/web_static/shared/" ]; then
-    mkdir -p "=/data/web_static/shared/"
-fi
+# Create files
+mkdir -p "/data/web_static/releases/test/"
+echo -e  "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" > "/data/web_static/releases/test/index.html"
+mkdir -p "=/data/web_static/shared/"
 
 # Create soft link from current to test directory
 ln -sf "/data/web_static/releases/test/" "/data/web_static/current"
@@ -25,7 +19,7 @@ ln -sf "/data/web_static/releases/test/" "/data/web_static/current"
 chown -R ubuntu:ubuntu /data/
 
 # Change Nginx configuration file
-IS_IN_FILE=$(grep -c "hbnb_static" "/etc/nginx/sites-available/default")
+IS_IN_FILE=$(grep -c hbnb_static /etc/nginx/sites-available/default)
 if [ "$IS_IN_FILE" -eq 0 ];
 then
    sed -i "/server_name _;/ a \\\n\tlocation /hbnb_static {\\n\\t\\talias /data/web_static/current/;\\n\\t}" "/etc/nginx/sites-available/default"
