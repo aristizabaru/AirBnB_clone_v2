358e42e3cd9013f9313aec585f2ff12824ff6153
aristizabaru <andres.aristizabal@gmail.com>
2021-04-16 02:44:11 +0000

    Fix: deploy with de_deploy() fixed

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,17 +4,9 @@ from fabric.api import *
 from datetime import datetime
 from os.path import isdir, getsize, isfile
 
-servers_list = {
-    "2210-web-01": "34.75.110.34",
-    "2210-web-02": "35.229.72.142"
-}
-
-env.roledefs = {
-    'servers': [servers_list['2210-web-01'], servers_list['2210-web-02']]
-}
+env.hosts = ["34.75.110.34", "35.229.72.142"]
 
 
-@runs_once
 def do_pack():
     """pack static_web"""
 
@@ -44,7 +36,6 @@ def do_pack():
     return "{}/{}".format(target_dir, file_name)
 
 
-@roles('servers')
 def do_deploy(archive_path):
     """deploy static"""
 
@@ -53,20 +44,20 @@ def do_deploy(archive_path):
         return False
     file_name = archive_path.split("/")[-1]
     name = file_name.split(".")[0]
-    # put(local, remote)
     try:
-        put(archive_path, "/tmp/{}".format(file_name))
+        put(archive_path, "/tmp/")
         run("mkdir -p /data/web_static/releases/{}".format(name))
         run("tar -xzf /tmp/{} -C "
-            "/data/web_static/releases/{}/".format(file_name,
-                                                   name))
+            "/data/web_static/releases/{}".format(file_name,
+                                                  name))
         run("rm /tmp/{}".format(file_name))
-        run("rsync -a /data/web_static/releases/{}/web_static/ "
+        run("mv /data/web_static/releases/{}/web_static/* "
             "/data/web_static/releases/{}/".format(
                 name, name))
         run("rm -rf /data/web_static/releases/{}/web_static".format(
             name))
-        run("ln -sf /data/web_static/releases/{}/ "
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ "
             "/data/web_static/current".format(
                 name))
         print("New version deployed!")
@@ -76,7 +67,6 @@ def do_deploy(archive_path):
         return False
 
 
-@roles('servers')
 def deploy():
     """deploy packing"""
     archive_path = do_pack()
