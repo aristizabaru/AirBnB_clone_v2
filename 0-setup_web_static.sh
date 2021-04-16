4c6caa20834c5371e6ac09e701e8dcf6c8b41133
aristizabaru <andres.aristizabal@gmail.com>
2021-04-16 01:05:33 +0000

    Fix: remove comments

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -18,7 +18,7 @@ fi
 
 # Create directory shared
 DIRECTORY=/data/web_static/shared/
-if ! [ -d "$DIRECTORY" ]; then
+if [ -d "$DIRECTORY" ]; then
     mkdir -p "$DIRECTORY"
 fi
 
@@ -39,21 +39,3 @@ then
    sed -i "/server_name _;/ a $DATA" "$CONFIG_WEB"
    nginx -s reload
 fi
-
-#!/usr/bin/env bash
-# This script sets up your web servers for the deployment of web_static
-# apt-get update
-# mkdir -p /data/
-# mkdir -p /data/web_static/
-# mkdir -p /data/web_static/releases/
-# mkdir -p /data/web_static/shared/
-# mkdir -p /data/web_static/releases/test/
-# echo " <h1 align=\"center\"> Cool You Nginx configuration is ready 🦾</h1> " > /data/web_static/releases/test/index.html
-# ln -s -f /data/web_static/releases/test/ /data/web_static/current
-# chown -R ubuntu:ubuntu /data/
-# # Install Nginx in my server
-# apt-get -y install nginx
-# # config hbnb_static as alias
-# sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
-# # restarts the server
-# service nginx restart
