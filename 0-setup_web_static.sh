9b6416b421db28b3b2f053f41d928008624c131e
aristizabaru <andres.aristizabal@gmail.com>
2021-04-16 02:02:45 +0000

    Fix: put again conditional

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,50 @@
 #!/usr/bin/env bash
 # Install Nginx
-apt-get update -y
-apt-get install nginx -y
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-printf "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+if ! [ -x "$(command -v nginx)" ]
+then
+   sudo apt-get update -y
+   sudo apt-get install nginx -y
+   sudo service nginx start
+fi
+
+# Create directories and index.html
+FILE=/data/web_static/releases/test/index.html
+DATA="<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>"
+FILE_DIRECTORY=/data/web_static/releases/test/
+if ! [ -f "$FILE" ]; then
+    mkdir -p "$FILE_DIRECTORY"
+    printf  "$DATA" > "$FILE"
+fi
+
+# Create directory shared
+DIRECTORY=/data/web_static/shared/
+if ! [ -d "$DIRECTORY" ]; then
+    mkdir -p "$DIRECTORY"
+fi
+
+# Create soft link from current to test directory
+LINK=/data/web_static/current
+TARGET=/data/web_static/releases/test/
+ln -sf "$TARGET" "$LINK"
+
+# Change owner
 chown -R ubuntu:ubuntu /data/
-sed -i "/server_name _;/ a location /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
-service nginx restart
+
+# Change Nginx configuration file
+CONFIG_WEB=/etc/nginx/sites-available/default
+DATA="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
+IS_IN_FILE=$(grep -c "hbnb_static" $CONFIG_WEB)
+if [ "$IS_IN_FILE" -eq 0 ];
+then
+   sed -i '/server_name _;/ a $DATA' "$CONFIG_WEB"
+   nginx -s reload
+fi
+
+#!/usr/bin/env bash
+# Install Nginx
+if ! [ -x "$(command -v nginx)" ]
+then
+   apt-get update -y
+   apt-get install nginx -y
+   service nginx start
+fi
