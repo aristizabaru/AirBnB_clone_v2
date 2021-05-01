b5b5e81db73cf804c0fa24a06cf91303588e852a
aristizabaru <andres.aristizabal@gmail.com>
2021-05-01 19:16:00 +0000

    Refactor: change name of variable 'number' for 'n'

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -33,7 +33,7 @@ def main():
 
     @app.route('/number_template/<int:n>', strict_slashes=False)
     def number_template(n):
-        return render_template('5-number.html', number=n)
+        return render_template('5-number.html', n=n)
 
     app.run(host='0.0.0.0', port='5000')
 
