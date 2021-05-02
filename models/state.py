2267591d1b15e22e38c6682099f163e5bde61853
aristizabaru <andres.aristizabal@gmail.com>
2021-05-02 13:28:56 +0000

    Fix: fix city state return on filestorage

--- models/state.py
+++ models/state.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
+from models.city import City
+from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
@@ -17,30 +20,14 @@ class State(BaseModel, Base):
     else:
         name = ""
 
-    @property
-    def cities(self):
-        """getter attribute cities that returns the
-            list of City instances with state_id"""
-        from models import storage
-        state_cities = list()
-        all_cities = storage.all("City")
-        for key, value in all_cities.items():
-            if value.state_id == self.id:
-                state_cities.append(value)
-        return state_cities
-
-    # __tablename__ = "states"
-    # name = Column(String(128), nullable=False)
-    # cities = relationship("City", backref="state", cascade="delete")
-    # if getenv("HBNB_TYPE_STORAGE") != "db":
-    #     @property
-    #     def cities(self):
-    #         """getter attribute cities that returns the
-    #         list of City instances with state_id"""
-    #         from models import storage
-    #         state_cities = list()
-    #         all_cities = storage.all("City")
-    #         for key, value in all_cities.items():
-    #             if value.state_id == self.id:
-    #                 state_cities.append(value)
-    #         return state_cities
+        @property
+        def cities(self):
+            """getter attribute cities that returns the
+                list of City instances with state_id"""
+            from models import storage
+            state_cities = list()
+            all_cities = storage.all(City)
+            for key, value in all_cities.items():
+                if value.state_id == self.id:
+                    state_cities.append(value)
+            return state_cities
